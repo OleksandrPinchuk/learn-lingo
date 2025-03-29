9c6aa9ee81bb5aee00312e40d1db0cccd8020b35
OleksandrPinchuk <116524115+OleksandrPinchuk@users.noreply.github.com>
2025-03-30 00:48:22 +0100

    Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,23 +4,34 @@ import icons from "/symbol-defs.svg";
 const Home = () => {
     return (
         <div className="container">
-            <div>
-                <div>
-                    <div>
-                        <h2>Unlock your potential with the best  language tutors</h2>
-                        <p>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
-                        <button>Get started</button>
+            <div className={css.wrapper}>
+                <div className={css.box}>
+                    <div className={css.content}>
+                        <h2 className={css.title}>Unlock your potential with the best  language tutors</h2>
+                        <p className={css.message}>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
+                        <button className={css.button}>Get started</button>
                     </div>
-                    <div>
-                        {/* <img src="/public/home-orange.svg" alt="logo" width="568" height="530" className={css.svg}/> */}
-                        <img src={`${icons}#icon-home-blue`} alt="logo" width="568" height="530" className={css.svg}/>
-                        <svg className={css.svg}>
-                            {/* <use href={`${icons}#icon-home-blue`} /> */}
-                            {/* <use href="/public/home-orange.svg" /> */}
-                        </svg>
-                    </div>
-                    
+                    <img src="/home-orange.svg" alt="girl at the computer" className={css.svg} />
                 </div>
+                <ul className={css.statistic}>
+                    <li className={css.item}>
+                        <p className={css.number}>32,000 +</p>
+                        <p className={css.text}>Experienced tutors</p>
+                    </li>
+                        <li className={css.item}>
+                        <p className={css.number}>300,000 +</p>
+                        <p className={css.text}>5-star tutor reviews</p>
+                    </li>
+                    <li className={css.item}>
+                        <p className={css.number}>120 +</p>
+                        <p className={css.text}>Subjects taught</p>
+                    </li>
+                    <li className={css.item}>
+                        <p className={css.number}>200 +</p>
+                        <p className={css.text}>Tutor nationalities</p>
+                    </li>
+                </ul>
+                
             </div>
         </div>
     )
