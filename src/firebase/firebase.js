305776ac19273b49d2ac179662913de22006ece7
OleksandrPinchuk <116524115+OleksandrPinchuk@users.noreply.github.com>
2025-05-28 23:58:32 +0200

    auth

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,11 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+
+
 const API_KEY = import.meta.env.VITE_API_KEY;
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+const DATABASE_URL = import.meta.env.VITE_DATABASE_URL;
 const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
 const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
 const MESSAGING_SUPER_ID = import.meta.env.VITE_MESSAGING_SUPER_ID;
@@ -10,6 +14,7 @@ const APP_ID = import.meta.env.VITE_APP_ID;
 const firebaseConfig = {
     apiKey: API_KEY,
     authDomain: AUTH_DOMAIN,
+    databaseURL: DATABASE_URL,
     projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
     messagingSenderId: MESSAGING_SUPER_ID,
@@ -17,5 +22,5 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-console.log(db)
+export const auth = getAuth(app);
+export const db = getDatabase(app);
