d7a7e2da99fb1c739f4a93c6b80a615fb4f80e31
OleksandrPinchuk <116524115+OleksandrPinchuk@users.noreply.github.com>
2025-06-02 22:40:16 +0200

    login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+        await signInWithEmailAndPassword(auth, email, password);
+        navigate("/dashboard");
+        } catch (error) {
+        alert(error.message);
+        }
+    };
+
+    return (
+        <form onSubmit={handleLogin}>
+        <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+        </form>
+    );
+}
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,36 @@
+
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { ref, set } from "firebase/database";
+import { auth, db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        try {
+        const userCred = await createUserWithEmailAndPassword(auth, email, password);
+        await set(ref(db, "users/" + userCred.user.uid), {
+            username,
+            email
+        });
+        navigate("/dashboard");
+        } catch (error) {
+        alert(error.message);
+        }
+    };
+
+    return (
+        <form onSubmit={handleRegister}>
+        <input placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+        <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Register</button>
+        </form>
+    );
+}
